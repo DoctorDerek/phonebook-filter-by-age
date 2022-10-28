d76eb918abbcc9985dd881c6c219f0cf8ba6bf51
Dr. Derek Austin <derekraustin@gmail.com>
2022-10-28 12:38:12 -0500

    chore(deps): configure Tailwind CSS using some boilerplate to extend the default spacing styles in the `tailwind.config.js`

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      spacing: {
+        15: "3.75rem",
+        96: "24rem",
+        104: "26rem",
+        112: "28rem",
+        114: "28.5rem",
+        116: "29rem",
+        120: "30rem",
+        128: "32rem",
+        144: "36rem",
+        160: "42rem",
+        176: "46rem",
+        192: "50rem",
+        208: "54rem",
+        224: "58rem",
+      },
+    },
+  },
+  plugins: [],
+}
