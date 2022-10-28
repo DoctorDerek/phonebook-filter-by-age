b53fb666622e1245ac28a14414ec33eb46c71ed4
Dr. Derek Austin <derekraustin@gmail.com>
2022-10-28 15:23:51 -0500

    fix(filter): change "placeholder:pl-6" to "pl-6" to correct the input text placement for the search filter

--- pages/index.tsx
+++ pages/index.tsx
@@ -68,7 +68,7 @@ export default function PhoneBookApp() {
         <input
           type="text"
           placeholder="Search for contact by last name..."
-          className="border-sm w-full rounded-md border border-gray-300 placeholder:pl-6 placeholder:text-sm placeholder:font-medium placeholder:text-gray-500"
+          className="border-sm w-full rounded-md border border-gray-300 pl-6 placeholder:text-sm placeholder:font-medium placeholder:text-gray-500"
           onChange={(event) => setFilterText(event?.target?.value)}
         />
         <MagnifyingGlassIcon className="absolute left-1 top-1/2 h-4 w-4 -translate-y-1/2" />
