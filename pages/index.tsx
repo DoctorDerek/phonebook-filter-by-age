178d6f69c3e8c78a34e0610cf734e2b7bc2ea16a
Dr. Derek Austin <derekraustin@gmail.com>
2022-10-28 18:47:11 -0500

    fix(tel): change `phoneNumber` type from "text" to "tel" and add a code comment about telephone validations

--- pages/index.tsx
+++ pages/index.tsx
@@ -196,11 +196,18 @@ export default function PhoneBookApp() {
                     <label className="flex space-x-1 text-sm text-gray-700">
                       <span>Phone Number</span>
                       <input
-                        type="text"
+                        type="tel"
                         placeholder={dialogState.phoneBookEntry?.phoneNumber}
                         {...register("phoneNumber", {
                           required: dialogState.type === "CREATE",
                         })}
+                        /**
+                         * Note that we don't try to validate the phone number,
+                         * because formats for telephone numbers vary so much
+                         * around the world. For example, many validations for
+                         * telephone numbers will reject any country code, such
+                         * as +1 for United States, even though that is wrong.
+                         * */
                         className="w-full rounded-md border border-gray-300 pl-1 placeholder:text-sm placeholder:font-medium placeholder:text-gray-500"
                         disabled={dialogState.type === "DELETE"}
                       />
