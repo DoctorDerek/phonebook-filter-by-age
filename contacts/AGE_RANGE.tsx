fefdf9fcbb1041d3d31cad4878c5d08dc22e7045
Dr. Derek Austin <derekraustin@gmail.com>
2022-11-11 13:19:37 -0600

    feat(contacts): upload the `AGE_RANGE` const from the "interview-f" source repository

---
+++ contacts/AGE_RANGE.tsx
@@ -0,0 +1,9 @@
+export type AgeRange = { label: string; rangeBottom: number; rangeTop: number }
+/** This is the required "age range" filter for the contacts list. */
+export const AGE_RANGE: AgeRange[] = [
+  { label: "Babies", rangeBottom: 0, rangeTop: 2 },
+  { label: "Children", rangeBottom: 3, rangeTop: 16 },
+  { label: "Young Adults", rangeBottom: 17, rangeTop: 29 },
+  { label: "Middle Aged Adults", rangeBottom: 30, rangeTop: 59 },
+  { label: "Seniors", rangeBottom: 60, rangeTop: 200 },
+]
