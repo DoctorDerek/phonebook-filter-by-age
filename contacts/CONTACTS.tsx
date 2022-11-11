13b766427c07a862f6d4263a72aa401b50ff1e71
Dr. Derek Austin <derekraustin@gmail.com>
2022-11-11 13:29:38 -0600

    feat(contacts): sort the `CONTACTS` by last name so we'll have a default sort of our contact list

--- contacts/CONTACTS.tsx
+++ contacts/CONTACTS.tsx
@@ -80,7 +80,7 @@ const CONTACTS: Contact[] = [
   },
 ]
 
-function calculateAge({ birthday }: { birthday: Date }) {
+const calculateAge = ({ birthday }: { birthday: Date }) => {
   const today = new Date()
   const age = today.getFullYear() - birthday.getFullYear()
   // This may not be their age, if their birthday hasn't yet occurred this year.
@@ -97,5 +97,20 @@ const CONTACTS_WITH_AGES: Contact[] = CONTACTS.map((contact) => {
   const age = calculateAge({ birthday })
   return { ...contact, age }
 })
+
+// Let's pre-sort the contacts before we use them, so we'll have a default sort.
+const sortByLastName = (a: Contact, b: Contact) => {
+  // We're going to assume that the last name is the last word in the name, but
+  // that may be incorrect, especially with many Asian or Hispanic names.
+  const aName = a?.name || ""
+  const bName = b?.name || ""
+  // We .pop() to get the last word in the name, and then we use .toLowerCase():
+  const aLastName = aName.split(" ").pop()?.toLocaleLowerCase() || ""
+  const bLastName = bName.split(" ").pop()?.toLocaleLowerCase() || ""
+  return aLastName.localeCompare(bLastName)
+}
+
+// We mutate the CONTACTS_WITH_AGES array to sort it by last name before export.
+CONTACTS_WITH_AGES.sort(sortByLastName)
 
 export default CONTACTS_WITH_AGES
