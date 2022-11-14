b719b7639021fea144e1237249d3d8c1802f364e
Dr. Derek Austin <derekraustin@gmail.com>
2022-11-13 18:11:42 -0600

    fix(Head): move `<title>` tag to the `@/app/head.tsx` file as its no longer supported to set it in the `<Head>` component in Next 13

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,8 @@
-/** The <head> shared between all pages, with utf-8, viewport, and favicon. */
+/** The <head> shared between all pages: title, utf-8, viewport, and favicon. */
 export default async function Head() {
   return (
     <>
+      <title>Phonebook &ldquo;Filter by Age&rdquo; App by @DoctorDerek</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
       <link rel="icon" href="/favicon.ico" />
--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import Head from "next/head"
-
 import PhoneBookApp from "@/components/PhoneBookApp"
 
 /** Creating routes inside `app/` requires a single file, `page.tsx`: */
 export default function Homepage() {
   return (
     <>
-      <Head>
-        <title>Phonebook &ldquo;Filter by Age&rdquo; App by @DoctorDerek</title>
-      </Head>
       <PhoneBookApp />
     </>
   )
