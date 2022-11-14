134376facee7120bfcef79f966c12f98c23dd339
Dr. Derek Austin <derekraustin@gmail.com>
2022-11-13 23:04:58 -0600

    refactor(ContactDialogInputs): create reusable `<ContactDialogInput>` in the `<ContactDialogInputs>` component to handle the various contact's `fieldName`

--- components/ContactDialogInputs.tsx
+++ components/ContactDialogInputs.tsx
@@ -3,6 +3,45 @@ import { FieldErrorsImpl, UseFormRegister } from "react-hook-form"
 import { DialogState } from "@/components/ContactDialog"
 import { Contact } from "@/contacts/CONTACTS"
 
+/**
+ * We have a helper for inputs (contact's `fieldName`).
+ *
+ * Note that we don't try to validate a phone number,
+ * because formats for telephone numbers vary so much
+ * around the world. For example, many validations for
+ * telephone numbers will reject any country code, such
+ * as +1 for United States, even though that is wrong.
+ * */
+function ContactDialogInput({
+  label,
+  fieldName,
+  dialogState,
+  register,
+}: {
+  label: string
+  fieldName: keyof Contact
+  dialogState: DialogState
+  register: UseFormRegister<Contact>
+}) {
+  /** We check for undefined and then coerce to a string. */
+  const placeholder = String(dialogState.contact?.[fieldName] || "")
+  return (
+    <label className="flex space-x-1 text-sm text-gray-700">
+      <span>{label}</span>
+      <input
+        type="text"
+        placeholder={placeholder}
+        {...register(fieldName, {
+          required: dialogState.type === "CREATE",
+        })}
+        className="w-full rounded-md border border-gray-300  pl-1 placeholder:text-sm placeholder:font-medium placeholder:text-gray-500"
+        disabled={dialogState.type === "DELETE"}
+      />
+    </label>
+  )
+}
+
+/** This is the "interior" of the form, where the user inputs data. */
 export default function ContactDialogInputs({
   dialogState,
   register,
@@ -17,37 +56,18 @@ export default function ContactDialogInputs({
       {dialogState.type !== "RESET" && (
         <>
           <div className="space-y-1 whitespace-nowrap">
-            <label className="flex space-x-1 text-sm text-gray-700">
-              <span>Name</span>
-              <input
-                type="text"
-                placeholder={dialogState.contact?.name}
-                {...register("name", {
-                  required: dialogState.type === "CREATE",
-                })}
-                className="w-full rounded-md border border-gray-300  pl-1 placeholder:text-sm placeholder:font-medium placeholder:text-gray-500"
-                disabled={dialogState.type === "DELETE"}
-              />
-            </label>
-            <label className="flex space-x-1 text-sm text-gray-700">
-              <span>Phone Number</span>
-              <input
-                type="tel"
-                placeholder={dialogState.contact?.phoneNumber}
-                {...register("phoneNumber", {
-                  required: dialogState.type === "CREATE",
-                })}
-                /**
-                 * Note that we don't try to validate the phone number,
-                 * because formats for telephone numbers vary so much
-                 * around the world. For example, many validations for
-                 * telephone numbers will reject any country code, such
-                 * as +1 for United States, even though that is wrong.
-                 * */
-                className="w-full rounded-md border border-gray-300 pl-1 placeholder:text-sm placeholder:font-medium placeholder:text-gray-500"
-                disabled={dialogState.type === "DELETE"}
-              />
-            </label>
+            <ContactDialogInput
+              label="Name"
+              fieldName="name"
+              dialogState={dialogState}
+              register={register}
+            />
+            <ContactDialogInput
+              label="Phone Number"
+              fieldName="phoneNumber"
+              dialogState={dialogState}
+              register={register}
+            />
             {dialogState.type === "CREATE" &&
               (errors.name || errors.phoneNumber) && (
                 <div>All fields are required.</div>
